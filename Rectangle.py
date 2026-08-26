3922fc499194688e021b5d9763370fcf93742c31
SalmanKhan <salmankhan2002.26@gmail.com>
2026-08-26 17:21:32 -0400

    Fixing and testing

--- Rectangle.py
+++ Rectangle.py
@@ -1,4 +1,4 @@
 height = float(input("Please enter your height in meters: "))
 width = float(input("Please enter your width in meters: "))
 area = height * width
-print("The area of the rectangle is:", area, "square meters.")
+print(f"The area of the rectangle is: {area} square meters.")
