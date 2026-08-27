5d152256fda09e32eb97453bb38fe3b4243b1c06
SalmanKhan <salmankhan2002.26@gmail.com>
2026-08-27 09:32:16 -0400

    Compound interest calculator

--- Test.py
+++ Test.py
@@ -1,5 +1,25 @@
-name = input("Enter name:")
-while(name==""):
-    name = input("Enter name:")
+capital =0
+rate=0
+years=0
 
-print(name)
+while True:
+    capital=float(input("Insert capital: "))
+    if capital<0:
+        print("Capital can't be less than zero.")
+    else:
+        break
+while True:
+    rate=float(input("Insert rate: "))
+    if rate<0:
+        print("Interest rate can't be less than zero.")
+    else:
+        break
+while True:
+    years=float(input("Insert years: "))
+    if years<0:
+        print("Years can't be less than zero.")
+    else:
+        break
+interest=(1+(rate/100))** years
+interest*=capital
+print(f"${interest:.2f}")
